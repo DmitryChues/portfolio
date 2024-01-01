a6405410622ccbba2ab1133229354201aacc208f
Dmitry <dmitrychues2312@gmail.com>
2024-01-02 01:40:04 +0300

    scrollToTop

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import { Icon } from "../icon/Icon";
 import { myTheme } from "../../styles/Theme.Styled";
 import styled from "styled-components";
+import { animateScroll as scroll } from "react-scroll";
 
 export const Logo = () => {
 	return (
-		<Link href="#">
-			<Icon hover={myTheme.colors.font.light} iconId={'logo'} viewBox="0 0 436 200" width="80" height="40px" fill={myTheme.colors.accent} />
+		<Link onClick={() => { scroll.scrollToTop() }}>
+			<Icon iconId={'logo'} viewBox="0 0 436 200" width="80" height="40px" />
 		</Link>
 	)
 }
 
 const Link = styled.a`
+	color: ${myTheme.colors.accent};
 	font-size: 0;
-	
+	cursor: pointer;
+	&:hover {
+		color: ${myTheme.colors.buttonHover};
+	}
 `
